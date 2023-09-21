dcf6d3d089c0d42bf6792db3e6eb16ebe18d4b5f
Amr Gaber <amrgaber2503@gmail.com>
2023-09-21 21:38:09 +0300

    Add files via upload

--- Python def Automate/automate_regs_def.py
+++ Python def Automate/automate_regs_def.py
@@ -2,71 +2,145 @@ import csv
 from datetime import date
 import time
 
-typedef_template_for_defs_with_bits_defs = '/*NameToBeReplaced Address Access Macro*/\n#define NameToBeReplaced_REG      (*(volatile NameToBeReplaced_reg_type * const)0xAddressToBeReplaced)\n'
-typedef_template_for_defs_without_bits_defs = '#define NameToBeReplaced_REG      (*(volatile uint8 * const)0xAddressToBeReplaced)\n'
-unions_template = '/*Register: {Name}*/\ntypedef union{{\n\tuint8 Byte;\n\tstruct{{\n\t\tuint8 {Bit0} :1;\n\t\tuint8 {Bit1} :1;\n\t\tuint8 {Bit2} :1;\n\t\t\
-uint8 {Bit3} :1;\n\t\tuint8 {Bit4} :1;\n\t\tuint8 {Bit5} :1;\n\t\tuint8 {Bit6} :1;\n\t\tuint8 {Bit7} :1;\n\t}}Bits;\n}}{Name}_reg_type;\n'
+# Create all variables needed later
+templates = {}
+template_key = ''
+template_lines_count = 0
+temp_template = ''
+temp_read_line = ''
+hundred_spaces_for_Alignment = ' '*100
+registers_with_bits_defs = {}
+registers_without_bits_defs = {}
+header_file_name = ''
+my_name = 'Amr Gaber'
+temp_txt = ''
 
-alot_of_spaces = '                                                                              '
-temp_str = ''
+with open('templates.txt', 'r') as templates_file:
+    # read the first line in the file to be used first in the while loop
+    temp_read_line = templates_file.readline()
+    while temp_read_line:
+        # read template address
+        temp_read_line = templates_file.readline()
+        template_key = temp_read_line.strip()
 
-result_file_name = 'sfr_defs_{NOW}.h'.format(NOW = date.today().strftime('%y_%m_%d_') + time.strftime("%H_%M_%S", time.localtime()))
+        # read how many lines in this template
+        temp_read_line = templates_file.readline()
+        template_lines_count = int(temp_read_line.replace('Lines: ', '').strip())
 
-defs_dicts_with_bits_defs = []
-defs_dicts_without_bits_defs = []
-with open('regs.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+        # read templates
+        temp_template = ''
+        for i in range(0, template_lines_count):
+            temp_read_line = templates_file.readline()
+            temp_template = temp_template + temp_read_line
+        temp_read_line = templates_file.readline()
+        templates[template_key] = temp_template
+
+with open('regs.csv', 'r') as registers_csv_file:
+    # open registers file as a csv object
+    csv_reader = csv.reader(registers_csv_file)
+
+    # Ignore first csv line cause it containes columns names
     next(csv_reader, None)
-    for line in csv_reader:
-        if len(line) == 10:
-            temp_reg = {'Address': line[0].split('$')[1], 'Name': line[1], 'Bit7': line[2],\
-                        'Bit6': line[3], 'Bit5': line[4], 'Bit4': line[5], 'Bit3': line[6],\
-                            'Bit2': line[7], 'Bit1': line[8], 'Bit0': line[9]}
-            defs_dicts_with_bits_defs.append(temp_reg)
-        if len(line) == 3:
-            temp_reg = {'Address': line[0].split('$')[1], 'Name': line[1], 'description': line[2]}
-            defs_dicts_without_bits_defs.append(temp_reg)
 
-with open(result_file_name, 'w') as sfr_defs_update_handler:
-    sfr_defs_update_handler.write('/*==========================================================================================\n\
-Name        : sfr_defs_update.h\n\
-Author      : Amr Gaber\n\
-Description : C Header file for the Atmega32\n\
-Date        : {DATE}\n\
-============================================================================================\*/\n\
-#ifndef SFR_DEFS_H_\n\
-#define SFR_DEFS_H_\n\n'.format(DATE = date.today().strftime('%d/%m/%Y')))
+    # iterate over each line in csv file and make a dict out of each line to use in templates
+    for register in csv_reader:
+        #for registers with special names for each bit
+        if len(register) == 10:
+            # Unify pins names in gpio registers
+            if (register[1])[0:4] == 'PORT':
+                for i in range(2, 10):
+                    register[i] = register[i].replace('PORT', 'PIN')
+            elif (register[1])[0:3] == 'DDR':
+                for i in range(2, 10):
+                    register[i] = register[i].replace('DD', 'PIN')
+            # add the register as a dict to a dict of dicts specific for
+            # registers with special names for each bit
+            registers_with_bits_defs[register[1]] = {'register_address': register[0].replace('$', ''),\
+                'register_name': register[1], 'register_bit7': register[2], 'register_bit6': register[3],\
+                'register_bit5': register[4], 'register_bit4': register[5], 'register_bit3': register[6],\
+                'register_bit2': register[7], 'register_bit1': register[8], 'register_bit0': register[9]}
 
-    sfr_defs_update_handler.write('typedef unsigned char uint8;\n')
-    for reg in defs_dicts_with_bits_defs:
-        if ((reg['Name'])[0:3] == 'DDR') or ((reg['Name'])[0:4] == 'PORT') or ((reg['Name'])[0:3] == 'PIN'):
-            for i in range(0,8):
-                # reg['Bit{i}'.format(i=i)] = 'PIN{i}'.format(i = i)
-                reg['Bit{i}'.format(i=i)] = 'PIN'+(reg['Bit{i}'.format(i=i)])[(len(reg['Bit{i}'.format(i=i)])-2):len(reg['Bit{i}'.format(i=i)])]
-        sfr_defs_update_handler.write(unions_template.format(Name = reg['Name'], Bit0 = reg['Bit0'], Bit1 = reg['Bit1'], Bit2 = reg['Bit2'],\
-            Bit3 = reg['Bit3'], Bit4 = reg['Bit4'], Bit5 = reg['Bit5'], Bit6 = reg['Bit6'], Bit7 = reg['Bit7']).replace('- ', ''))
-        sfr_defs_update_handler.write(typedef_template_for_defs_with_bits_defs.replace('NameToBeReplaced',\
-                    reg['Name']).replace('AddressToBeReplaced' , reg['Address']))
-        sfr_defs_update_handler.write('/*{Name} Bit Position Macros*/\n'.format(Name = reg['Name']))
-        for i in range(0,8):
-            if reg['Bit{i}'.format(i=i)] != '-':
-                temp_str = '#define _{register}_{bit}_POSITION'.format(register = reg['Name'], bit = reg['Bit{i}'.format(i=i)])
-                temp_str = temp_str + alot_of_spaces
-                temp_str = temp_str[0:30] + '0x{i}\n'.format(i = i)
-                sfr_defs_update_handler.write(temp_str)
+        #for registers with no bits names
+        if len(register) == 3:
+            # add the register as a dict to a dict of dicts specific for
+            # registers with no bits names
+            registers_without_bits_defs[register[1]] = {'register_address': register[0].replace('$', ''),\
+                'register_name': register[1], 'register_description': register[2]}
 
-        sfr_defs_update_handler.write('/*{Name} Bit mask Macros*/\n'.format(Name = reg['Name']))
-        for i in range(0,8):
-            if reg['Bit{i}'.format(i=i)] != '-':
-                temp_str = '#define _{register}_{bit}_MASK'.format(register = reg['Name'], bit = reg['Bit{i}'.format(i=i)])
-                temp_str = temp_str + alot_of_spaces
-                temp_str = temp_str[0:30] + '{i}\n'.format(i = hex(pow(2,i)))
-                sfr_defs_update_handler.write(temp_str)
-        sfr_defs_update_handler.write('\n\n')
+# open a new header file with a unique name generated time specific
+# to avoid overwriting
+header_file_name = 'register_definition_file_TIME_PLACEHOLDER.h'.replace('TIME_PLACEHOLDER', \
+    date.today().strftime('%y_%m_%d_') + time.strftime("%H_%M_%S", time.localtime()))
 
-    sfr_defs_update_handler.write('\n')
+with open(header_file_name, 'w') as register_definition_file:
+    register_definition_file.write(templates['HEADER_FILE_HEADER_TEMPLATE']\
+        .replace('FILE_NAME_PLACEHOLDER', header_file_name)\
+        .replace('AUTHOR_PLACEHOLDER', my_name)\
+        .replace('DATE_PLACEHOLDER', date.today().strftime('%d/%m/%Y ') +\
+        time.strftime("%H:%M:%S", time.localtime())))
+    # register_definition_file.write('\n')
 
-    for reg in defs_dicts_without_bits_defs:
-        sfr_defs_update_handler.write('/*{Description}*/\n'.format(Description = reg['description']))
-        sfr_defs_update_handler.write(typedef_template_for_defs_without_bits_defs.replace('NameToBeReplaced',  reg['Name']).replace('AddressToBeReplaced' , reg['Address']) + '\n')
-    sfr_defs_update_handler.write("#endif /* SFR_DEFS_H_ */")
+    register_definition_file.write(templates['FILE_GUARD_START']\
+        .replace('HEADERFILE_DEF_NAME_PLACEHOLDER', (header_file_name\
+            .upper().replace('.', '_')+'_')))
+    register_definition_file.write('\n')
+
+    register_definition_file.write('typedef unsigned char uint8;\n')
+    register_definition_file.write('\n')
+
+    for register in registers_with_bits_defs:
+        register_definition_file.write('/*Register: REGISTER_NAME_PLACEHOLDER*/\n'\
+            .replace('REGISTER_NAME_PLACEHOLDER', registers_with_bits_defs[register]['register_name']))
+
+        register_definition_file.write(templates['REGISTER_DEFINITION_TEMPLATE']\
+            .replace('REGISTER_NAME_PLACEHOLDER', registers_with_bits_defs[register]['register_name'])\
+            .replace('BIT0_PLACEHOLDER', registers_with_bits_defs[register]['register_bit0'])\
+            .replace('BIT1_PLACEHOLDER', registers_with_bits_defs[register]['register_bit1'])\
+            .replace('BIT2_PLACEHOLDER', registers_with_bits_defs[register]['register_bit2'])\
+            .replace('BIT3_PLACEHOLDER', registers_with_bits_defs[register]['register_bit3'])\
+            .replace('BIT4_PLACEHOLDER', registers_with_bits_defs[register]['register_bit4'])\
+            .replace('BIT5_PLACEHOLDER', registers_with_bits_defs[register]['register_bit5'])\
+            .replace('BIT6_PLACEHOLDER', registers_with_bits_defs[register]['register_bit6'])\
+            .replace('BIT7_PLACEHOLDER', registers_with_bits_defs[register]['register_bit7'])\
+            .replace('- ', ''))
+
+        register_definition_file.write('/*REGISTER_NAME_PLACEHOLDER Address Access Macro*/\n'\
+            .replace('REGISTER_NAME_PLACEHOLDER', registers_with_bits_defs[register]['register_name']))
+
+        register_definition_file.write(templates['REGISTER_WITH_BITS_DEFS_ADDRESS_ACCESS_TEMPLATE']\
+            .replace('REGISTER_NAME_PLACEHOLDER', registers_with_bits_defs[register]['register_name'])\
+            .replace('REGISTER_ADDRESS_PLACEHOLDER', registers_with_bits_defs[register]['register_address']))
+
+        register_definition_file.write('/*REGISTER_NAME_PLACEHOLDER Bit Macros*/\n'\
+            .replace('REGISTER_NAME_PLACEHOLDER', registers_with_bits_defs[register]['register_name']))
+        for i in range(0, 8):
+            if registers_with_bits_defs[register]['register_bit'+str(i)] != '-':
+                temp_txt =  templates['REGISTER_BIT_POSITION_MACRO_TEMPLATE']\
+                    .replace('REGISTER_NAME_PLACEHOLDER', registers_with_bits_defs[register]['register_name'])\
+                    .replace('BIT_NAME_PLACEHOLDER', registers_with_bits_defs[register]['register_bit'+str(i)])\
+                    .replace('BIT_POSITION_PLACEHOLDER', '').strip()
+                temp_txt = temp_txt + (40 - len(temp_txt))*' ' + 'BIT_POSITION_PLACEHOLDER\n'
+                temp_txt = temp_txt.replace('BIT_POSITION_PLACEHOLDER', '0x'+str(i))
+                register_definition_file.write(temp_txt)
+
+                temp_txt = ''
+                temp_txt =  templates['REGISTER_BIT_MASK_MACRO_TEMPLATE']\
+                    .replace('REGISTER_NAME_PLACEHOLDER', registers_with_bits_defs[register]['register_name'])\
+                    .replace('BIT_NAME_PLACEHOLDER', registers_with_bits_defs[register]['register_bit'+str(i)])\
+                    .replace('BIT_MASK_PLACEHOLDER', '').strip()
+                temp_txt = temp_txt + (40 - len(temp_txt))*' ' + 'BIT_MASK_PLACEHOLDER\n'
+                temp_txt = temp_txt.replace('BIT_MASK_PLACEHOLDER', str(hex(pow(2,i))))
+                register_definition_file.write(temp_txt)
+        register_definition_file.write('\n\n')
+    for register in registers_without_bits_defs:
+        register_definition_file.write('/*DESCRIPTION_PLACEHOLDER*/\n'\
+            .replace('DESCRIPTION_PLACEHOLDER', registers_without_bits_defs[register]['register_description']))
+
+        register_definition_file.write(templates['REGISTER_WITHOUT_BITS_DEFS_ADDRESS_ACCESS_TEMPLATE']\
+            .replace('REGISTER_NAME_PLACEHOLDER', registers_without_bits_defs[register]['register_name'])\
+            .replace('REGISTER_ADDRESS_PLACEHOLDER', registers_without_bits_defs[register]['register_address']))
+        register_definition_file.write('\n')
+
+    register_definition_file.write(templates['FILE_GUARD_END']\
+        .replace('HEADERFILE_DEF_NAME_PLACEHOLDER', (header_file_name\
+            .upper().replace('.', '_')+'_')))
